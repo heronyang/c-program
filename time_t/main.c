f58ba69f749fdb2d5b244fa566b1ae1294903f11
Heron Yang <heronyang@cmu.edu>
2016-11-23 08:06:19 -0700

    test print

--- time_t/main.c
+++ time_t/main.c
@@ -14,6 +14,18 @@ int main() {
     printf("size of time_t = %zu, int = %zu, long = %zu\n",
             sizeof(time_t), sizeof(int), sizeof(long));
 
+
+    char buff[20];
+    strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&now));
+    printf("Time (time_t): %s\n", buff);
+
+    // ERROR while compiling
+    //strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&int_now));
+    //printf("Time (int): %s\n", buff);
+
+    strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&long_now));
+    printf("Time (long): %s\n", buff);
+
     return 0;
 
 }
